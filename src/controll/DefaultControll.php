d9dc10d33917a0bdd837ee7237c30982c2a67cc4
softluc <softluc@softluc-Crestline-ICH8M-Chipset>
2013-03-11 22:53:52 -0300

    TESTE DE COMMIT NETBEANS

--- src/controll/DefaultControll.php
+++ src/controll/DefaultControll.php
@@ -1,159 +1,130 @@
 <?php
+
 /**
  * Classe DefaultControll
  * Controlador default da aplicação
  * @package controll
  */
-	class DefaultControll extends Controll {
-		
-		/**
-		 * Acao index()
-		 */
-		public function index(){
-			//$this->setTela(($this->getUsuario()) ? 'home' : 'login');
-			$this->setTela(($this->getUsuario()) ? 'home' : 'loginTesteAndroid');
-			$this->getPage();
-		}
-		
-		/**
-		 * Acao logar()
-		 * Verifica se foram passados os dados do formulário POST
-		 */
-		public function logar(){
-			parent::getUsuario() ? $this->setTela('home')
-			   				     : ($this->getDados('POST') ? $this->_logar($this->getDados('POST'))
-														    : $this->setTela('login')); 
-		}
-		
-		/**
-		 * Metodo _logar($dados)
-		 * Persiste em logar o usuário com os dados passados por parametro no formulário
-		 * @param $dados
-		 */		
-		private function _logar($dados){
-			/**
-			 * Persistindo em logar
-			 */
-			try {
-				$usuario = Usuario::logar($dados['login'],$dados['senha']);
-				//guardando o usuário no controlador
-				$this->setUsuario($usuario);
-				//recuperando se houver alguma url guardada
-				$urlRecover = $this->getUrlRecover();
-				//redirecionando
-				header("Location: " . (($urlRecover) ? $urlRecover : 'index'));
-			}
-			
-			/**
-			 * Capturando a excessão CamposObrigatorios
-			 */
-			catch(CamposObrigatorios $e){
-				$this->setFlash($e->getMessage());
-				$this->setTela('login');
-			}
-			
-			/**
-			 * Capturando a excessão LoginInvalido
-			 */
-			catch(LoginInvalido $e){
-				$this->setFlash($e->getMessage());
-				$this->setTela('login');
-			}
-		}
-		
-		/**
-		 * Acao logout()
-		 * Destroi a sessão e redireciona para a tela default de login
-		 */
-		public function logout(){
-			session_destroy();
-			header("Location: index");
-		}
-		
-		public function voltar(){
-			$this->setPage();
-		}
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                /*PARA ANDROID*/
-                public function logarAndroid(){
-                    if($this->getDados('POST')){
-                        $this->_logarAndroid($this->getDados('POST'));
-                    }
-                    
-			   		
-		}
-		
-				
-		private function _logarAndroid($dados){
-                        
-                    try {
-                        
-                        $arrayRetorno = Usuario::logarAndroid($dados['login'],$dados['senha']);
+class DefaultControll extends Controll {
 
-                        $arrayRetorno["status"] = 0;
-                        $arrayRetorno["messagem"] = "OK";
-                        
-                        header('Cache-Control: no-cache, must-revalidate');
-                        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');                    
-                        header('Content-type: application/json');
+    /**
+     * Acao index()
+     */
+    public function index() {
+        //$this->setTela(($this->getUsuario()) ? 'home' : 'login');
+        $this->setTela(($this->getUsuario()) ? 'home' : 'loginTesteAndroid');
+        $this->getPage();
+    }
 
-                        echo json_encode($arrayRetorno);
-                    }
-                    
-                    catch(CamposObrigatorios $e){
-                        
-                        $arrayRetorno["status"] = 1;
-                        $arrayRetorno["messagem"] = $e->getMessage();
-                        
-                        header('Cache-Control: no-cache, must-revalidate');
-                        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');                    
-                        header('Content-type: application/json');
+    /**
+     * Acao logar()
+     * Verifica se foram passados os dados do formulário POST
+     */
+    public function logar() {
+        parent::getUsuario() ? $this->setTela('home') : ($this->getDados('POST') ? $this->_logar($this->getDados('POST')) : $this->setTela('login'));
+    }
 
-                        echo json_encode($arrayRetorno);
-                    }
-                    
-                    catch(LoginInvalido $e){
-                        $arrayRetorno["status"] = 1;
-                        $arrayRetorno["messagem"] = $e->getMessage();
-                        
-                        header('Cache-Control: no-cache, must-revalidate');
-                        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');                    
-                        header('Content-type: application/json');
+    /**
+     * Metodo _logar($dados)
+     * Persiste em logar o usuário com os dados passados por parametro no formulário
+     * @param $dados
+     */
+    private function _logar($dados) {
+        /**
+         * Persistindo em logar
+         */
+        try {
+            $usuario = Usuario::logar($dados['login'], $dados['senha']);
+            //guardando o usuário no controlador
+            $this->setUsuario($usuario);
+            //recuperando se houver alguma url guardada
+            $urlRecover = $this->getUrlRecover();
+            //redirecionando
+            header("Location: " . (($urlRecover) ? $urlRecover : 'index'));
+        }
 
-                        echo json_encode($arrayRetorno);
-                    }
-                    
-                    catch(Exception $e){
-                        $arrayRetorno["status"] = 1;
-                        $arrayRetorno["messagem"] = $e->getMessage();
-                        
-                        header('Cache-Control: no-cache, must-revalidate');
-                        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');                    
-                        header('Content-type: application/json');
+        /**
+         * Capturando a excessão CamposObrigatorios
+         */ catch (CamposObrigatorios $e) {
+            $this->setFlash($e->getMessage());
+            $this->setTela('login');
+        }
 
-                        echo json_encode($arrayRetorno);
-                    }
-                    
-		}
-                /*PARA ANDROID*/
-                
-	}
+        /**
+         * Capturando a excessão LoginInvalido
+         */ catch (LoginInvalido $e) {
+            $this->setFlash($e->getMessage());
+            $this->setTela('login');
+        }
+    }
+
+    /**
+     * Acao logout()
+     * Destroi a sessão e redireciona para a tela default de login
+     */
+    public function logout() {
+        session_destroy();
+        header("Location: index");
+    }
+
+    public function voltar() {
+        $this->setPage();
+    }
+
+    /* PARA ANDROID */
+
+    public function logarAndroid() {
+        if ($this->getDados('POST')) {
+            $this->_logarAndroid($this->getDados('POST'));
+        }
+    }
+
+    private function _logarAndroid($dados) {
+
+        try {
+
+            $arrayRetorno = Usuario::logarAndroid($dados['login'], $dados['senha']);
+
+            $arrayRetorno["status"] = 0;
+            $arrayRetorno["messagem"] = "OK";
+
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            header('Content-type: application/json');
+
+            echo json_encode($arrayRetorno);
+        } catch (CamposObrigatorios $e) {
+
+            $arrayRetorno["status"] = 1;
+            $arrayRetorno["messagem"] = $e->getMessage();
+
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            header('Content-type: application/json');
+
+            echo json_encode($arrayRetorno);
+        } catch (LoginInvalido $e) {
+            $arrayRetorno["status"] = 1;
+            $arrayRetorno["messagem"] = $e->getMessage();
+
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            header('Content-type: application/json');
+
+            echo json_encode($arrayRetorno);
+        } catch (Exception $e) {
+            $arrayRetorno["status"] = 1;
+            $arrayRetorno["messagem"] = $e->getMessage();
+
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            header('Content-type: application/json');
+
+            echo json_encode($arrayRetorno);
+        }
+    }
+
+    /* PARA ANDROID */
+}
+
 ?>
