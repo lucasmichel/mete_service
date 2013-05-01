c236784687c8f51ad79641a7bdfe58d3b0e33692
lopes <lopes@lopes-PC.Home>
2013-05-01 14:49:25 -0300

    Alteracao na classe localizacao

--- src/view/localizacao/listar.php
+++ src/view/localizacao/listar.php
@@ -1,97 +1,48 @@
-<?php
-header('Content-Type: text/html; charset=utf-8', true);
-?>
-<div class="wrap">
-    <?php
-    include_once(VIEW . DS . "default" . DS . "tops" . DS . "servico.php");
-    ?>
-    <div id="dashboard-wrap">
-        <div class="metabox"></div>
-        <!-- 
-        <div class="filtros">
-                <span class="select-all">
-                        <SELECT NAME="function">
-                                <OPTION VALUE="excluir">excluir todos</OPTION>
-                                <OPTION VALUE="movimentar">movimentar todos</OPTION>
-                        </SELECT>
-                        <br/>
-                </span>
-                <span class="select-all">
-                        <SELECT NAME="function">
-                                <OPTION VALUE="excluir">excluir todos</OPTION>
-                                <OPTION VALUE="movimentar">movimentar todos</OPTION>
-                        </SELECT>
-                        <br/>
-                </span>
-                <span class="select-all busca">
-                        <input type="text" value="Buscar..." />
-                        <input type="button" value="ir"/>
-                        <br/>
-                </span>
-        </div> fim div filtro-->
-        <div class="clear"> </div>
-        <div class="box-content">
-            <div class="box">
-                <?php
-                /**
-                 * Persistindo em listar os usuÃ¡rios
-                 */
-                try {
-                    $objetos = Servico::listar("nome");
-                    $paginacao = new Paginacao($objetos, 20);
-                    ?>
-                    <div class="table">
-                        <table id="lista" class="widefat fixed">
-                            <thead>
-                                <tr>
-                                    <th width="1%"><input type="checkbox" id="all" style="visibility:hidden;"/></th>
-                                    <th width="1%"></th>
-                                    <th width="28%" align="left">Nome</th>                                    
-                                    <th width="20%" align="left">AÃ§Ãµes</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                foreach ($paginacao->getDados() as $objeto) {
-                                    ?>
-                                    <tr>
-                                        <th width="1%">
-                                            <input type="checkbox" id="ids" name="ids[]" value="" style="visibility:hidden;"/>
-                                        </th>
-                                        <td width="1%"></td>
-                                        <td width="28%" align="left"><?php echo $objeto->getNome(); ?></td>
-                                        
-                                        <td width="20%">						
-                                            <a href="servico/ver/<?php echo $objeto->getId(); ?>">Ver</a> 
-                                            <?php
-                                            if (Acao::checarPermissao(2, ServicoControll::MODULO)) {
-                                                ?>
-                                                <a href="servico/editar/<?php echo $objeto->getId(); ?>">Editar</a> 
-                                                <a href="servico/excluir/<?php echo $objeto->getId(); ?>">Excluir</a>
-                                                <?php
-                                            }
-                                            ?>
-                                        </td>
-                                    </tr>
-                                    <?php
-                                }
-                                ?>
-                            </tbody>
-                        </table>
-                    </div><!--fim div table-->
-                    <div class="table-footer">
-                        <span class="page"><?php echo $paginacao->getLinks(); ?></span>
-                    </div>
-                    <?php
-                } catch (Exception $e) {
-                    ?>
-                    <div class="exception">
-                        <?php echo $e->getMessage(); ?>
-                    </div>
-                    <?php
-                }
-                ?>
-            </div> <!--fim div box-->
-        </div> <!--fim div box-content-->
-    </div><!--fim div dashboard-wrap-->
-</div><!--fim div wrap-->
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+ "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
+  <title>Localização do usuário via Geo IP</title>
+  <style type="text/css" media="screen">
+
+  </style>
+</head>
+<body>
+<div id="doc">
+  <div id="map"></div>
+  <div  id="info"></div>
+</div>
+<script type="text/javascript" src="http://j.maxmind.com/app/geoip.js"></script>
+<script>
+(function(){
+
+  var info = document.getElementById('info');
+  var lat = geoip_latitude();
+  var lon = geoip_longitude();
+  var city = geoip_city();
+  
+  var out = '<h3>Informacoes para seu IP</h3>'+ 
+            '<ul>'+
+            '<li>Latitude: ' + lat + '</li>'+
+            '<li>Longitude: ' + lon + '</li>'+
+            '<li>Cidade: ' + city + '</li>'+
+            '<li>Cod. Regiao: ' + geoip_region() + '</li>'+
+            '<li>Regiao: ' + geoip_region_name() + '</li>'+
+            '<li>Codigo do Pais: ' + geoip_country_code() + '</li>'+
+            '<li>Nome do Pais: ' + geoip_country_name() + '</li>'+
+            '</ul>';
+  info.innerHTML = out;
+  var url = 'http://maps.google.com/maps/api/staticmap?center='+
+            lat+','+lon+'&sensor=false&size=300x300&maptype=roadmap&key='+
+            'ABQIAAAAijZqBZcz-rowoXZC1tt9iRT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQQBCa'+
+            'F1R_k1GBJV5uDLhAKaTePyQ&markers=color:blue|label:I|'+lat+
+            ','+lon+'6&visible='+lat+','+lon+'|'+(+lat+1)+','+(+lon+1);
+  var map = document.getElementById('map');
+  map.innerHTML = '<img src="'+url+'" alt="'+city+'">';
+  
+})();
+</script>
+</body>
+</html>
