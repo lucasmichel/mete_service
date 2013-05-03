0f8e6cdad93fc41af36b879dcd92125d440bca4f
kaykylopes <kaykylopes@kaykylopes-PC>
2013-05-03 20:45:46 -0300

    alteracao

--- src/view/comentario/add.php
+++ src/view/comentario/add.php
@@ -16,7 +16,7 @@
             <div class="box">
                 <div class="table">
                     <h3 class="hndle">                        
-                        <span>Cadastrar Modulo Vai Vai</span>
+                        <span>Cadastrar Modulo bora</span>
                     </h3>
                     <div class="inside">
                         <form method="post" id="cadastro">
