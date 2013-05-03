2b03ac5a10c40435842fea14597addcd93987601
lucasmichel <lucasmic@gmail.com>
2013-05-03 16:28:37 -0300

    continuacao de cadstro de servico acompanhnate

--- src/view/acompanhante/servicos/add.php
+++ src/view/acompanhante/servicos/add.php
@@ -3,6 +3,74 @@
     $acompanhante = $this->getDados('acompanhante');
     $servicosAcompanhante = $this->getDados('servicosAcompanhante');
 ?>
+
+
+<script type="text/javascript" src="http://j.maxmind.com/app/geoip.js"></script>
+
+
+<style type="text/css">  
+    #googleMap { height: 100% }
+</style>
+
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDz-izkZlMj8pCSosNtyjeUhFiPYbIqfaU&sensor=false"></script>
+
+<script>
+/*pra pegar informações d alocalizaçãoda pessoa*/
+    var lat = geoip_latitude();
+    //alert(lat);
+    var lon = geoip_longitude();
+    //alert(lon);
+    var city = geoip_city();
+    //alert(city);
+/*pra pegar informações d alocalizaçãoda pessoa*/
+
+
+var map = null; 
+
+//var myCenter = new google.maps.LatLng(-8.102738577783168,-35.299072265625);
+var myCenter = new google.maps.LatLng(lat, lon);
+var marker;
+
+    function initialize()
+    {
+        var mapProp = {
+          center:myCenter,
+          zoom:12,
+          mapTypeId:google.maps.MapTypeId.ROADMAP
+          };
+
+        map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+        marker=new google.maps.Marker({
+          position:myCenter,
+          animation:google.maps.Animation.BOUNCE
+          });
+
+        marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+    function marcar(){
+        var endereco = document.getElementById("endereco").value ;        
+        geocoder = new google.maps.Geocoder();		
+        geocoder.geocode({'address':endereco}, function(results, status){ 
+            if( status = google.maps.GeocoderStatus.OK){
+
+                    alert(results[0].geometry.location);
+                    myCenter = results[0].geometry.location;
+                    markerInicio = new google.maps.Marker({position: myCenter,map: map});		
+                    map.setCenter(myCenter); 
+
+            }
+        });
+    }
+
+</script>
+
+
+
 <script type="text/javascript">
     $(document).ready(function($){
     	
@@ -51,9 +119,7 @@
     });
 </script>
 <div class="wrap">
-    <?php
-    include_once(VIEW . DS . "default" . DS . "tops" . DS . "acompanhante.php");
-    ?>
+    <h2><span>Serviços da acompanhante: <?php echo $acompanhante->getnome();?></span></h2>
     <div id="dashboard-wrap">
         <div class="metabox"></div>
         <div class="clear"></div>
@@ -61,7 +127,7 @@
             <div class="box">
                 <div class="table">
                     <h3 class="hndle">                        
-                        <span>Cadastrar serviço da acompanhante</span>
+                        <span>Cadastrar serviço</span>
                     </h3>
                     <div class="inside">
                         <form method="post" id="cadastro">
@@ -110,20 +176,16 @@
                                     </li>
                                     
                                     
-                                    <li>
-                                        <label for="senha">Serviço</label>
-                                        <input type="password" id="senha" name="senha" value="" class="required" />
-                                    </li>
+                                    
                                     <li>
                                         <label for="nome">Localização</label>
-                                        <input type="text" id="nome" name="nome" value="<?php if($acompanhante != null) echo $acompanhante->getNome();  ?>"  />
+                                        <div id="googleMap"></div>
+                                        <br />
+                                        <label>Pesquisar por endereço</label>
+                                        <input type="text" id="endereco" />
+                                        <input type="button" value="Marcar" id="marcar" onclick="marcar()">
                                     </li>
                                     
-                                    
-                                    
-                                    
-                                    
-                                     
                                     
                                 </ul>
                             </fieldset>
