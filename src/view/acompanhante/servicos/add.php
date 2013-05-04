40a1a9c47713b5e1cdd597cb6f8dc7421e059ed1
SoftLuc <softluc@softluc-Crestline-ICH8M-Chipset.(none)>
2013-05-04 01:56:51 -0300

    retirada de bug de google maps  ejquery

--- src/view/acompanhante/servicos/add.php
+++ src/view/acompanhante/servicos/add.php
@@ -13,17 +13,21 @@
 </style>
 
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDz-izkZlMj8pCSosNtyjeUhFiPYbIqfaU&sensor=false"></script>
-
+<script type="text/javascript">
+    $(document).ready(function(){    
+    $("#ok").click(function() {
+        alert("oi");
+    });
+});
+</script>
 <script>
-/*pra pegar informações d alocalizaçãoda pessoa*/
-    var lat = geoip_latitude();
-    //alert(lat);
-    var lon = geoip_longitude();
-    //alert(lon);
-    var city = geoip_city();
-    //alert(city);
-/*pra pegar informações d alocalizaçãoda pessoa*/
 
+    
+/*pra pegar informações d alocalizaçãoda pessoa*/
+  var lat = geoip_latitude();
+  var lon = geoip_longitude();
+  var city = geoip_city();
+/*pra pegar informações d alocalizaçãoda pessoa*/
 
 var map = null; 
 
@@ -31,93 +35,44 @@ var map = null;
 var myCenter = new google.maps.LatLng(lat, lon);
 var marker;
 
-    function initialize()
-    {
-        var mapProp = {
-          center:myCenter,
-          zoom:12,
-          mapTypeId:google.maps.MapTypeId.ROADMAP
-          };
+function initialize(){
+    var mapProp = {
+      center:myCenter,
+      zoom:12,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
 
-        map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-        marker=new google.maps.Marker({
-          position:myCenter,
-          animation:google.maps.Animation.BOUNCE
-          });
-
-        marker.setMap(map);
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-
-
-    function marcar(){
-        var endereco = document.getElementById("endereco").value ;        
-        geocoder = new google.maps.Geocoder();		
-        geocoder.geocode({'address':endereco}, function(results, status){ 
-            if( status = google.maps.GeocoderStatus.OK){
-
-                    alert(results[0].geometry.location);
-                    myCenter = results[0].geometry.location;
-                    markerInicio = new google.maps.Marker({position: myCenter,map: map});		
-                    map.setCenter(myCenter); 
-
-            }
-        });
-    }
-
-</script>
-
-
-
-<script type="text/javascript">
-    $(document).ready(function($){
-    	
-        
-        $('#email').focus();
-       
-                
-        $("#ok").click(function() {
-        	var senha = $.trim($("#senha").val());
-        	var email = $.trim($("#email").val());
-        	var nome = $.trim($("#nome").val());
-        	
-        	if(email.length <= 0){
-            	alert('é necessário um email');
-                $("#email").focus();
-                return false;
-			}
-        	            
-            else if(!validaEmail(email)){
-            	alert('email invalido');
-                $("#email").focus();
-                return false;
-			}
-
-            
-            else if(senha.length <= 0){
-                alert('é necessário definir a senha');
-                $("#senha").focus();
-                return false;
-            }
-
-            else if(nome.length <= 0){
-                alert('é necessário definir o nome');
-                $("#nome").focus();
-                return false;
-            }
-            
-            else{
-                $("#cadastro").submit();
-            }
-            
-          
-        });                
-                
-                
+    marker=new google.maps.Marker({
+      position:myCenter,
+      animation:google.maps.Animation.BOUNCE
     });
+
+    marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+function marcar(){
+    var endereco = document.getElementById("endereco").value ;        
+    geocoder = new google.maps.Geocoder();		
+    geocoder.geocode({'address':endereco}, function(results, status){ 
+        if( status = google.maps.GeocoderStatus.OK){
+            alert(results[0].geometry.location);
+            myCenter = results[0].geometry.location;
+            markerInicio = new google.maps.Marker({position: myCenter,map: map});		
+            map.setCenter(myCenter); 
+        }			
+    });
+}
+
+
+
 </script>
+
+
 <div class="wrap">
     <h2><span>Serviços da acompanhante: <?php echo $acompanhante->getnome();?></span></h2>
     <div id="dashboard-wrap">
@@ -132,16 +87,12 @@ var marker;
                     <div class="inside">
                         <form method="post" id="cadastro">
                             <input type="hidden" name="acompanhanteId" id="acompanhanteId" value="<?php echo $acompanhante->getId(); ?>" />
-                            <fieldset>
-                                <legend>Dados</legend>
+
                                 <ul class="list-cadastro">   
-                                    
                                     <li>
                                         <label for="email">Valor</label>
-                                        <input type="text" id="valor" name="email" value="<?php if($usuario != null) echo $usuario->getEmail();  ?>" />
+                                        <input alt="decimal" type="text" id="valor" name="email" value="<?php if($servicosAcompanhante != null) echo $servicosAcompanhante->getValor();  ?>" />
                                     </li>
-                                    
-                                    
                                     <li>
                                         <label for="perfil">Serviço</label>
                                         <select id="servico" name="servico" class="required">
@@ -165,36 +116,40 @@ var marker;
                                                         <?php
                                                     }
                                                 } catch (Exception $e) {
-                                                
+
                                                 ?>
                                                     <option value="erro" ><?php echo $e->getMessage();?></option>
                                                 <?php
-                                                
+
                                             }
                                             ?>
                                         </select>
                                     </li>
+                                
                                     
                                     
-                                    
-                                    <li>
+                                </ul>
+
+                                
+                        </form>
+                        
+                        <ul>
+                            <li>
                                         <label for="nome">Localização</label>
                                         <div id="googleMap"></div>
                                         <br />
                                         <label>Pesquisar por endereço</label>
                                         <input type="text" id="endereco" />
-                                        <input type="button" value="Marcar" id="marcar" onclick="marcar()">
-                                    </li>
-                                    
-                                    
-                                </ul>
-                            </fieldset>
-                            <ul id="bts">
-                                <li>
-                                    <input type="button" class="classBt" value=" OK " id="ok"/>
-                                </li>
-                            </ul>
-                        </form>
+                                        <input type="button" value="Marcar" class="classBt" onclick="marcar();">
+                            </li>
+                        </ul>
+                        
+                        <ul id="bts">
+                            <li>
+                                <input type="button" class="classBt" value=" OK " id="ok" name="ok"/>
+                            </li>
+                        </ul>
+                        
                     </div><!--fim div inside-->
                 </div><!--fim div table-->
                 <div class="table-footer"></div>
